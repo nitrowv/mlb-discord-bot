2f8e4620bb2fa463a7ef50e3abe588372ad9565d
Tanner Johnston <tjjohnston19@my.trine.edu>
2021-09-17 16:59:43 -0400

    made year optional for !standings, formatted file.

--- main.py
+++ main.py
@@ -17,35 +17,44 @@ bot = commands.Bot(command_prefix="!")
 today = date.today()
 todayDate = today.strftime("%m/%d/%Y")
 
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
 
 @bot.command(name='player')
 async def player(ctx, firstName, lastName):
     pid = playerid_lookup(lastName, firstName)
     await ctx.send(pid)
 
+
 @bot.command(name='prospects')
 async def prospects(ctx, team):
     data = top_prospects(team)
-    rank = data.iloc[0:9,0:1]
-    playerName = data.iloc[0:9,1:2]
-    age = data.iloc[0:9,2:3]
+    rank = data.iloc[0:9, 0:1]
+    playerName = data.iloc[0:9, 1:2]
+    age = data.iloc[0:9, 2:3]
     rank = rank.to_string(index=False, header=False)
     playerName = playerName.to_string(index=False, header=False)
-    age = age.to_string(index = False, header = False)
-    embed=discord.Embed(title=team.capitalize() + " Top 10 Prospects")
+    age = age.to_string(index=False, header=False)
+    embed = discord.Embed(title=team.capitalize() + " Top 10 Prospects")
     embed.add_field(name="Rank", value=rank, inline=True)
     embed.add_field(name="Player", value=playerName, inline=True)
     embed.add_field(name="Age", value=age, inline=True)
     await ctx.send(embed=embed)
 
+
 @bot.command(name='standings')
 async def divisionStandings(ctx, *args):
-    year = int(args[0])
-    league = args[1]
-    div = args[2]
+    if(len(args) == 3):
+        year = int(args[0])
+        league = args[1]
+        div = args[2]
+    else:
+        year = today.year()
+        league = args[0]
+        div = args[1]
     divDisplay = (league.upper() + " " + div.capitalize())
     if(league.upper() == "AL"):
         if(div.upper() == "EAST"):
@@ -60,7 +69,7 @@ async def divisionStandings(ctx, *args):
                 divPass = 1
             else:
                 divPass = 2
-    else:
+    elif(league.upper() == "NL"):
         if(div.upper() == "EAST"):
             if(year <= 1994):
                 divPass = 2
@@ -76,28 +85,31 @@ async def divisionStandings(ctx, *args):
                 divPass = 5
             else:
                 divPass = 3
+    else:
+        await ctx.send("You have entered an invalid league.")
     try:
         data = standings(year)[divPass]
         i = 0
         j = 1
         k = data.shape[0]
-        winLoss=""
-        teamList=""
-        for i in range(0,k):
-            teams = data.iloc[i:j,0:1]
-            wins = data.iloc[i:j,1:2]
-            losses = data.iloc[i:j,2:3]
-            teams = teams.to_string(index = False, header = False)
+        winLoss = ""
+        teamList = ""
+        for i in range(0, k):
+            teams = data.iloc[i:j, 0:1]
+            wins = data.iloc[i:j, 1:2]
+            losses = data.iloc[i:j, 2:3]
+            teams = teams.to_string(index=False, header=False)
             teamList += (teams + "\n")
-            wins = wins.to_string(index = False, header = False)
-            losses = losses.to_string(index = False, header = False)
+            wins = wins.to_string(index=False, header=False)
+            losses = losses.to_string(index=False, header=False)
             winLoss += wins + "-" + losses + "\n"
             j += 1
-        winPct = data.iloc[0:j,3:4]
-        winPct = winPct.to_string(index = False, header = False)
-        embed=discord.Embed(title=str(year) + " " + divDisplay + " Standings")
+        winPct = data.iloc[0:j, 3:4]
+        winPct = winPct.to_string(index=False, header=False)
+        embed = discord.Embed(title=str(year) + " " +
+                              divDisplay + " Standings")
         embed.add_field(name="Team", value=teamList, inline=True)
-        embed.add_field(name="W-L", value=winLoss, inline=True) 
+        embed.add_field(name="W-L", value=winLoss, inline=True)
         embed.add_field(name="Win Percentage", value=winPct, inline=True)
         await ctx.send(embed=embed)
     except UnboundLocalError:
@@ -120,14 +132,16 @@ async def dateGames(ctx, *args):
         currentTime = now.astimezone(timeZone)
         gameTime = datetime.strftime(gameLocal, '%I:%M %p %Z')
         if (gameLocal > currentTime):
-            gameText += (x['away_name'] + " at " + x['home_name'] + " - " + gameTime + "\n")
+            gameText += (x['away_name'] + " at " +
+                         x['home_name'] + " - " + gameTime + "\n")
         else:
             if(x['status'] == "In Progress"):
-                gameText += (x['away_name'] + " (" + str(x['away_score']) + ") at " + x['home_name'] + " (" + str(x['home_score']) + ") - " + x['inning_state'].capitalize() + x['current_inning'] + "\n")
+                gameText += (x['away_name'] + " (" + str(x['away_score']) + ") at " + x['home_name'] + " (" + str(
+                    x['home_score']) + ") - " + x['inning_state'].capitalize() + x['current_inning'] + "\n")
             else:
-                gameText += (x['away_name'] + " (" + str(x['away_score']) + ") at " + x['home_name'] + " (" + str(x['home_score']) + ") - " + x['status'] + "\n")
+                gameText += (x['away_name'] + " (" + str(x['away_score']) + ") at " +
+                             x['home_name'] + " (" + str(x['home_score']) + ") - " + x['status'] + "\n")
     await ctx.send(gameText.rstrip())
-
 
 
 bot.run(os.getenv('TOKEN'))
